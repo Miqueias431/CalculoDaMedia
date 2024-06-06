88abe0ba51ccf12c15dc8af4837d114b5783ad4d
Miqueias Rodrigues Barreto <miqueiasr560@gmail.com>
2024-06-06 17:01:58 -0300

    add

--- media.js
+++ media.js
@@ -45,19 +45,23 @@ function calcularStatus() {
   }
 
   function limparCampos(){
-    document.getElementById('nota1').value = ''
-    document.getElementById('nota2').value = ''
-    document.getElementById('nota3').value = ''
-    document.getElementById('nota4').value = ''
-
-    document.getElementById('status').textContent = ''
-    document.getElementById('imgStatus').src = 'img/inicio.png'
-    // document.getElementById('notaCount').textContent = ''
-    document.getElementById('mediaNota').textContent = ''
+    let btnLimpar = document.getElementById('btnLimpar')
+    btnLimpar.addEventListener('click', () => {
+        statusElement.textContent = ''
+        mediaNotaElement.textContent = ''
+        imgElement.src = 'img/inicio.png'
+        // notaCountElement.textContent = ''
+        document.getElementById('nota1').value = ''
+        document.getElementById('nota2').value = ''
+        document.getElementById('nota3').value = ''
+        document.getElementById('nota4').value = ''
+    })
   }
   
   // Chamar a função quando o botão for clicado
   document.getElementById('btnCalcular').addEventListener('click', calcularStatus)
 
-  document.getElementById('btnLimpar').addEventListener('click', limparCampos)
+  limparCampos() 
+  
+
   
